05d7f0e36ffaa6639b34291565b8b13bcf9d5436
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-02-05 11:54:47 +0000

    currently trying to resolve isse with calling the data from JSON file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import vinyl from './vinyl.png';
+// import axios from 'axios';
 import vinylcollection from './vinylcollection.png';
 import './App.css';
 import SearchForm from './Components/SearchForm.js';
 import NavBar from './NavBar/NavBar.js'; 
-// import { location } from './Data/stores.json';
+import LocationDetails from './Components/LocationDetails.js';
+import { LocationDetails } from './Data/stores.json';
 
 class App extends React.Component {
   constructor() {
@@ -14,22 +16,43 @@ class App extends React.Component {
       location: {
         city: '',
         country: '',
+        postcode: '',
       },
     };
+    // this.handleSearchStoresClick = this.handleSearchStoresClick.bind(this);
   };
-  handleSearchStoresClick(city) {
-    this.setState(state => ({
-        location: Response.data.location,
-    }))
-  }
-    render() {
+  // handleSearchStoresClick(city) {
+  //   this.setState(state => ({
+  //       location: Response.data.location,
+  //   }))
+  // }
+  // componentDidMount() {
+  //   axios.get('./Data/stores.json/location')
+  //   this.setState({
+  //     location: response.data.location
+  //   });
+  // };
+
+
+// handleSearchStoresClick(city, postcode) {
+//   axios
+//     .get('./Data/stores.json/city, postcode?city, postcode=${city, postcode}')
+//     .then(response => {
+//       location: response.data.location
+//     });
+//   };
+// };
+
+  render() {
       
   return (
     <div className="App">
       <header className="App-header">
         <NavBar/>
         <img src={vinylcollection} className="App-vinylcollection" alt="vinyls" />
-        
+        <LocationDetails name={this.state.location.name} 
+        city={this.state.location.city} 
+        postcode={this.state.location.postcode}/>
         <p>
             The Digger
         </p>
@@ -42,6 +65,7 @@ class App extends React.Component {
       </header>
     </div>
     )};
-  };
+};  
+
 
 export default App;
--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 class SearchForm extends React.Component {
   constructor(props) {
@@ -22,22 +22,24 @@ class SearchForm extends React.Component {
       <div className="searchForm">
         <form>
           <input
-            className="Enter postcode"
-            placeholder="Enter Postcode..."
+            className="Enter town or city"
+            placeholder="Enter town or city..."
             onChange={this.handleInputChange}
             value={this.state.searchText}
           />
       </form>
-      <button onClick={event => {
+      <button className="seachButton"
+      onClick={event => {
         event.preventDefault();
         this.props.handleSearchStoresClick(this.state.searchText);
-      }}>
-        Search</button>
+      }}>Search</button>
       </div>
-  );
+    );
   }
 }
-    
+SearchForm.propTypes = {
+  handleSearchStoresClick: PropTypes.func.isRequired,
+};    
 
 
 
